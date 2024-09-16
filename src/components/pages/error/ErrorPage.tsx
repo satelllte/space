7d2c28b4a54a78ba1579fe8a10792f5301fa2fa9
satelllte <20585619+satelllte@users.noreply.github.com>
2024-09-16 23:45:16 +0300

    Error pages (#2)
    error pages: 404, 500

---
+++ src/components/pages/error/ErrorPage.tsx
@@ -0,0 +1,16 @@
+import {Link} from '../../ui/Link';
+
+type ErrorPageProps = {
+  message: string;
+};
+
+export function ErrorPage({message}: ErrorPageProps) {
+  return (
+    <main className='flex min-h-full flex-col gap-4 px-4 pb-6 pt-10 sm:px-8 sm:pb-8 sm:pt-12'>
+      <h1 className='text-lg text-gray-12'>{message}</h1>
+      <Link size='xs' href='/'>
+        {'<- Back'}
+      </Link>
+    </main>
+  );
+}
