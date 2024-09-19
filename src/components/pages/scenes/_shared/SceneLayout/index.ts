5b793464049bee4d3c060aab68e707e0a85287df
Yehor <20585619+satelllte@users.noreply.github.com>
2024-09-19 23:07:07 +0300

    shared SceneLayout

---
+++ src/components/pages/scenes/_shared/SceneLayout/index.ts
@@ -0,0 +1,1 @@
+export {SceneLayout} from './SceneLayout';
--- src/components/pages/scenes/particles/SceneParticles.tsx
+++ src/components/pages/scenes/particles/SceneParticles.tsx
@@ -1,7 +1,7 @@
 import {OrbitControls} from '@react-three/drei';
 import {Theme} from '../../../context/Theme';
-import {Link} from '../../../ui/Link';
 import {Canvas} from '../_shared/Canvas';
+import {SceneLayout} from '../_shared/SceneLayout';
 import {Particles} from './Particles';
 
 export function SceneParticles() {
@@ -22,32 +22,5 @@ export function SceneParticles() {
         </Canvas>
       </SceneLayout>
     </Theme>
-  );
-}
-
-function SceneLayout({children}: {children: React.ReactNode}) {
-  return (
-    <>
-      <noscript>
-        <div className='fixed z-10 flex h-full w-full items-center justify-center'>
-          <p className='text-gray-12'>
-            {
-              'Cannot display the scene because JavaScript is disabled in this browser :('
-            }
-          </p>
-        </div>
-      </noscript>
-
-      {children}
-
-      <footer className='fixed bottom-6 left-4 right-4 z-20 flex items-end sm:bottom-8 sm:left-8 sm:right-8'>
-        <div /* This empty div is required for the link to be aligned the same way it appears on homepage */
-        >
-          <Link size='xs' href='/'>
-            {`<- Back`}
-          </Link>
-        </div>
-      </footer>
-    </>
   );
 }
