89cae15d5eb480b7e01ac7d45fb9e3575a029b67
Yehor <20585619+satelllte@users.noreply.github.com>
2024-09-22 00:23:46 +0300

    config: remove dotenv commented template code

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,5 @@
 import {defineConfig, devices} from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
 const ci = Boolean(process.env.CI);
 const port = process.env.PORT ?? 4321;
 const baseUrl = `http://localhost:${port}`;
