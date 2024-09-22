19fa0b640ca1e9b388eee6aa3ad16848317a357e
Yehor <20585619+satelllte@users.noreply.github.com>
2024-09-22 17:26:12 +0300

    tests

---
+++ tests-e2e/accessibility.spec.ts
@@ -0,0 +1,23 @@
+import {test, expect, type Page} from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
+import {SCENES} from './_constants';
+
+test("shouldn't have any auto detectable issues (home)", async ({page}) => {
+  await page.goto('/');
+  await expectNoIssues({page});
+});
+
+test("shouldn't have any auto detectable issues (scenes)", async ({page}) => {
+  test.fixme(); // TODO: fix issues
+
+  for (const scene of SCENES) {
+    const {href} = scene;
+    await page.goto(href);
+    await expectNoIssues({page});
+  }
+});
+
+const expectNoIssues = async ({page}: {page: Page}) => {
+  const results = await new AxeBuilder({page}).analyze();
+  expect(results.violations).toEqual([]);
+};
