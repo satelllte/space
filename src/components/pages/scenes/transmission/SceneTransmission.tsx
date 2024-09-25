3571078f9d23df2a4804c5ac2f2f0e13e8b1ae92
Yehor <20585619+satelllte@users.noreply.github.com>
2024-09-25 23:55:31 +0300

    WIP: low res transmission

--- src/components/pages/scenes/transmission/SceneTransmission.tsx
+++ src/components/pages/scenes/transmission/SceneTransmission.tsx
@@ -5,16 +5,29 @@ import {OrbitControls} from '../_shared/OrbitControls';
 import {Environment} from './Environment';
 import {Lighting} from './Lighting';
 import {TransmissionObject} from './TransmissionObject';
+import {Scroll, ScrollControls} from '@react-three/drei';
 
 export function SceneTransmission() {
   return (
     <Theme>
       <SceneLayout>
-        <Canvas camera={{position: [0.0, 0.0, 3.4]}}>
-          <OrbitControls />
+        <Canvas
+          camera={{position: [0.0, 0.0, 1.7]}}
+          // style={{filter: 'grayscale(1.0)'}}
+        >
+          <OrbitControls minDistance={1.7} maxDistance={1.7} />
           <Lighting />
           <Environment />
+          <mesh position={[0.0, 0.0, 0.0]}>
+            <boxGeometry args={[0.5, 0.5, 0.5]} />
+            <meshPhysicalMaterial color={0xff0000} />
+          </mesh>
           <TransmissionObject />
+          {/* <ScrollControls pages={2} damping={0.1}>
+            <Scroll>
+              <TransmissionObject />
+            </Scroll>
+          </ScrollControls> */}
         </Canvas>
       </SceneLayout>
     </Theme>
--- src/components/pages/scenes/transmission/TransmissionObject/TransmissionObject.tsx
+++ src/components/pages/scenes/transmission/TransmissionObject/TransmissionObject.tsx
@@ -1,28 +1,51 @@
 import {useRef} from 'react';
-import {useFrame} from '@react-three/fiber';
-import {MeshTransmissionMaterial} from '@react-three/drei';
+import {useFrame, useThree} from '@react-three/fiber';
+import {MeshTransmissionMaterial, useScroll} from '@react-three/drei';
+import {degToRad} from 'three/src/math/MathUtils.js';
 
 export function TransmissionObject() {
   const meshRef = useRef<React.ElementRef<'mesh'>>(null);
+  // const scroll = useScroll();
+  // const viewport = useThree((three) => three.viewport);
+  console.debug('scroll: ', scroll);
 
-  useFrame((_, timeDelta) => {
+  useFrame(({clock}, timeDelta) => {
     const mesh = meshRef.current;
     if (!mesh) return;
 
     mesh.rotation.y += timeDelta * 0.11;
     mesh.rotation.z += timeDelta * 0.03;
+    // mesh.position.y = Math.sin(clock.elapsedTime * 2.0) * 0.5;
+
+    // mesh.rotation.y = scroll.offset
+    // console.debug('scroll.offset: ', scroll.offset);
+    // mesh.position.y = viewport.height * scroll.offset;
+    // mesh.rotation.z = scroll.offset;
+    // mesh.scale.x = 1.0 + scroll.offset;
+    // mesh.scale.y = 1.0 + scroll.offset;
+    // mesh.scale.z = 1.0 + scroll.offset;
   });
 
   return (
-    <mesh ref={meshRef}>
-      <boxGeometry />
+    <mesh
+      ref={meshRef}
+      // rotation={[0.0, degToRad(90.0), degToRad(45.0)]}
+    >
+      {/* <torusKnotGeometry args={[1.1, 0.4, 72, 16]} /> */}
+      <sphereGeometry args={[1.0, 64, 64]} />
+      {/* <boxGeometry /> */}
+      {/* <tetrahedronGeometry /> */}
       <MeshTransmissionMaterial
-        thickness={0.2}
-        chromaticAberration={0.9}
+        // wireframe
+        // transmission={1.2}
+        thickness={0.1}
+        chromaticAberration={0.2}
         backside
         samples={4}
-        distortion={0.8}
-        roughness={0.1}
+        distortion={10200.8}
+        roughness={10.0}
+        resolution={8.0}
+        backsideResolution={4.0}
       />
     </mesh>
   );
